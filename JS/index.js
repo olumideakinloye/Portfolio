4168ecfef1e5622416248e557dcae2afa872ab42
olumideakinloye <olumideakinloye24@gmail.com>
2026-02-14 14:54:05 +0100

    Finished technical skills section

--- JS/index.js
+++ JS/index.js
@@ -37,17 +37,88 @@ function type() {
 
   setTimeout(type, isDeleting ? deletingSpeed : typingSpeed);
 }
-function adjustAboutSectionHeight(){
+function adjustAboutSectionHeight() {
   const aboutContent = document.getElementById("about-content");
   const aboutSection = document.getElementById("about");
-  const aboutBG = document.getElementById("about-BG")
-  if(aboutContent.clientHeight > window.innerHeight){
-  aboutSection.style.height = `${aboutContent.clientHeight}px`;
-  aboutBG.style.height = `${aboutContent.clientHeight}px`;
+  const observer = new ResizeObserver(() => {
+    const childBottom = aboutContent.offsetTop + aboutContent.offsetHeight;
+    aboutSection.style.minHeight = childBottom + "px";
+    // aboutBG.style.minHeight = Math.max(window.innerHeight, childBottom) + "px";
+  });
+
+  observer.observe(aboutContent);
+}
+
+function adjustSkillsSectionHeight() {
+  const skillsContent = document.getElementById("skills-content");
+  const skillsSection = document.getElementById("skills");
+  const observer = new ResizeObserver(() => {
+    const childBottom = skillsContent.offsetTop + skillsContent.offsetHeight;
+    skillsSection.style.minHeight = childBottom + "px";
+    // aboutBG.style.minHeight = Math.max(window.innerHeight, childBottom) + "px";
+  });
+
+  observer.observe(skillsContent);
+}
+
+function adjustAOSDelays() {
+  const cards = document.querySelectorAll(".about-skill-card");
+  if (window.innerWidth < 890) {
+    if (window.innerWidth < 640) {
+      cards.forEach((card) => {
+        if (card.hasAttribute("data-aos-delay")) {
+          card.removeAttribute("data-aos-delay");
+        }
+      });
+    } else {
+      cards.forEach((card, index) => {
+        if(!card.hasAttribute("data-aos-delay")){
+          if (index == 1) {
+            card.setAttribute("data-aos-delay", "400");
+          }
+        }else{
+          if(index == 2){
+            card.removeAttribute("data-aos-delay");
+          }
+        }
+      });
+    }
+  } else {
+    cards.forEach((card, index) => {
+      if (!card.hasAttribute("data-aos-delay")) {
+        if (index == 1) {
+          card.setAttribute("data-aos-delay", "400");
+        } else if (index == 2) {
+          card.setAttribute("data-aos-delay", "800");
+        }
+      }
+    });
   }
 }
-window.addEventListener("resize", adjustAboutSectionHeight);
-adjustAboutSectionHeight();
 
+const boxes = document.querySelectorAll(".bubble-3d");
+
+boxes.forEach((box) => {
+  const randomDelay = (Math.random() * 10 - 5).toFixed(2); // 0 to 5 seconds
+  box.style.animationDelay = `${randomDelay}s`;
+  box.querySelector(".image-wrapper").style.animationDelay = `${randomDelay}s`;
+  box.querySelector(".image-wrapper-2").style.animationDelay =
+    `${randomDelay}s`;
+  box.querySelector("img").style.animationDelay = `${randomDelay}s`;
+});
+
+window.addEventListener("load", () => {
+  adjustAboutSectionHeight();
+  adjustSkillsSectionHeight();
+  adjustAOSDelays();
+});
+window.addEventListener("resize", () => {
+  adjustAboutSectionHeight();
+  adjustSkillsSectionHeight();
+  adjustAOSDelays();
+});
+adjustAboutSectionHeight();
+adjustSkillsSectionHeight();
+adjustAOSDelays();
 // Initial start delay
 setTimeout(type, 1000);
